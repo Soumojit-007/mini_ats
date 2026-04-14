827837aeabfc498414d8fa9c7b20d46094af410c
Soumojit-007 <sanjugon2003@gmail.com>
2026-04-14 12:34:43 +0530

    push

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import app from "./app.js";
 import { config } from "./config/config.js";
-
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });
